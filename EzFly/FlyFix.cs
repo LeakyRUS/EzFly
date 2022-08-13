c7ac22b84343cf4a4954a631d6df17bad6621a57
LeakyRUS <vladislav.zhilin@gmail.com>
2022-08-14 01:14:08 +0300

    Добавьте файлы проекта.

---
+++ EzFly/FlyFix.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using HarmonyLib;
+
+using ABI.CCK.Components;
+using ABI_RC.Systems.MovementSystem;
+
+namespace EzFly
+{
+    public class FlyFix : MelonMod
+    {
+        [HarmonyPatch(typeof(CVRWorld), "SetupWorldRules")]
+        public class Fix
+        {
+            static void Postfix()
+            {
+                MovementSystem.Instance.canFly = true;
+            }
+        }
+    }
+}
